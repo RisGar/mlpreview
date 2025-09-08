222331cbe93d1e36269b058fc9732170c0b1f449
Rishab Garg <me@rishab-garg.me>
2025-09-08 16:42:52 +0200

    feat: add git symbols to directories

--- lib/directory.ml
+++ lib/directory.ml
@@ -1,7 +1,7 @@
 let directory () =
   let stream =
     Unix.open_process_in
-      "eza -a1F --color=always --group-directories-first --icons"
+      "eza -a1F --color=always --group-directories-first --icons=always --git"
   in
   stream |> In_channel.input_all |> print_endline;
   In_channel.close stream
