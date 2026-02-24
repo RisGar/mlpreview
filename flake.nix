e67cbc8b0894ceb07d17c03ff84159f822e7e07e
Rishab Garg <mail@rishab-garg.de>
2026-02-24 02:23:29 +0100

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "mlpreview";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
